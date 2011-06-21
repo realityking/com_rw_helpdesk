d8d97e8594078d6f5608ae3f98bc41eecbb77426
Rouven Weßling <me@rouvenwessling.de>
2011-06-21 22:10:55 +0200

    Fix the controller loading.

--- admin/helpdesk.php
+++ admin/helpdesk.php
@@ -8,26 +8,10 @@ if (!JFactory::getUser()->authorise('core.manage', 'com_helpdesk')) {
 	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
 }
 
-// Require the base controller
-require_once(JPATH_COMPONENT.'/controller.php');
+jimport('joomla.application.component.controller');
+
 require_once(JPATH_COMPONENT.'/helpers/helpdesk.php');
 
-// Require specific controller if requested
-if($controller = JRequest::getWord('controller')) {
-    $path = JPATH_COMPONENT.'/controllers/'.$controller.'.php';
-    if (file_exists($path)) {
-        require_once $path;
-    } else {
-        $controller = '';
-    }
-}
-
-// Create the controller
-$classname    = 'HelpdeskController'.$controller;
-$controller   = new $classname( );
-
-// Perform the Request task
-$controller->execute( JRequest::getVar( 'task' ) );
-
-// Redirect if set by the controller
+$controller	= JController::getInstance('Helpdesk');
+$controller->execute(JRequest::getCmd('task'));
 $controller->redirect();
