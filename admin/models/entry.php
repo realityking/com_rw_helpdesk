7a779fc28c6036dada736b62522e860a128e3cce
Rouven Weßling <me@rouvenwessling.de>
2011-06-21 21:07:57 +0200

    Remove uneccesarry stuff from HelpdeskModelEntry.

--- admin/models/entry.php
+++ admin/models/entry.php
@@ -13,51 +13,6 @@ jimport('joomla.application.component.modeladmin');
  */
 class HelpdeskModelEntry extends JModelAdmin
 {
-	var $_data = null;
-	var $_id = null;			
-
-	/**
-	 * Constructor that retrieves the ID from the request
-	 *
-	 * @access    public
-	 * @return    void
-	 */
-	function __construct()
-	{
-	    parent::__construct();
-	
-	    $array = JRequest::getVar('cid',  0, '', 'array');
-	    $this->setId((int)$array[0]);
-	}
-
-	/**
-	 * Method to auto-populate the model state.
-	 *
-	 * Note. Calling getState in this method will result in recursion.
-	 *
-	 * @since	1.6
-	 */
-	protected function populateState()
-	{
-		// Load state from the request.
-		$pk = JRequest::getInt('id');
-		$this->setState('entry.id', $pk);
-	}
-
-	/**
- 	* Method to set the entry identifier
- 	*
- 	* @param    int Entry identifier
- 	*
- 	* @return    void
- 	*/
-	public function setId($id)
-	{
-    	// Set id and wipe data
-    	$this->_id        = $id;
-    	$this->_data    = null;
-	}
-
 	// Publishes an entry or unpublishes it
 	public function publish($state)
 	{
